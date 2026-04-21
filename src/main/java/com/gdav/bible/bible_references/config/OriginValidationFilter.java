bccbbb7aa42e5618c60a4ce50aceed0d4b9b3adc
Dave G <gdav4610>
2026-04-21 14:59:15 -0600

    Changes to origin

--- src/main/java/com/gdav/bible/bible_references/config/OriginValidationFilter.java
+++ src/main/java/com/gdav/bible/bible_references/config/OriginValidationFilter.java
@@ -27,7 +27,7 @@ public class OriginValidationFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        String origin = request.getHeader("Origin");
+        String origin = request.getHeader("X-Client-Origin");
 
         // Ahora bloqueamos si no viene header Origin
         if (origin == null || origin.isBlank()) {
